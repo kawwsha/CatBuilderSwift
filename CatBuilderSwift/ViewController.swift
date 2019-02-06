a2d51e8d9963ce1db5042c9d5d9ca0b22fd031cd
Kasia McLellan <kmclella@villanova.edu>
2019-02-05 22:26:51 -0500

    Add picture

--- CatBuilderSwift/ViewController.swift
+++ CatBuilderSwift/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var nameLabel: UILabel!
+    private let smoothieCat = Cat(name: "Smoothie") //Age defaults to 0
+    private var melkaCat: Cat?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let whiteCoffeeCat = Cat(name: "White Coffee Cat", age:6)
+        var smoothie = UIImage(named: "Image")
+        var smoothieImageView = UIImageView(image: smoothie)
+        smoothieCat.celebrateBirthday()
+        whiteCoffeeCat.meow()
+        melkaCat = Cat(name: "Melka", age: 3)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        nameLabel.text = smoothieCat.name
+        printMelkaCat()
+        //Never use ! in your code, only permissable in IBOutlet
+    }
+    
+    private func printMelkaCat(){
+        guard let melkaCat = melkaCat else {return}
+        print(melkaCat.name)
+        }
 
 }
 
+// MARK: - Cat class (should be in seperate file)
+final class Cat {
+    
+    // MARK: Public Properties
+    let name: String
+    private(set) var age: Int //Publically visible, but can only be set privately
+    
+    var ageInHumanYears: Int {
+        return age * 8 // A custom getter
+    }
+    
+    // MARK: Initialization
+    init(name: String, age: Int = 0) {
+        self.name = name
+        self.age = age
+    }
+    
+    // MARK: Public Action Methods
+    func celebrateBirthday() {
+        age += 1
+    }
+    
+    func meow() {
+        print("MEOWWWW")
+    }
+}

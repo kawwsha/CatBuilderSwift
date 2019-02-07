93bed0f69a34de94fbda0224d5e8903846596a7c
Kasia McLellan <kmclella@villanova.edu>
2019-02-06 23:19:54 -0500

    Fix viewDidLoad method

--- CatBuilderSwift/CatTableViewController.swift
+++ CatBuilderSwift/CatTableViewController.swift
@@ -15,6 +15,7 @@ class CatTableViewController: UITableViewController {
     var cats = [Cat]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSampleCats()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
